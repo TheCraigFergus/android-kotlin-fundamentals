e34f42d9228acadd04d2ed466f0d829dae4add87
Hank Chan <hankchan@cubesole.ai>
2021-03-16 01:11:24 +0800

    RecyclerView exercise

--- RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -31,177 +31,116 @@ import kotlinx.coroutines.launch
  * ViewModel for SleepTrackerFragment.
  */
 class SleepTrackerViewModel(
-        dataSource: SleepDatabaseDao,
-        application: Application) : ViewModel() {
+		dataSource: SleepDatabaseDao,
+		application: Application) : ViewModel() {
 
-    /**
-     * Hold a reference to SleepDatabase via SleepDatabaseDao.
-     */
-    val database = dataSource
+	val database = dataSource
+	private var tonight = MutableLiveData<SleepNight?>()
+	val nights = database.getAllNights()
 
-    private var tonight = MutableLiveData<SleepNight?>()
+	val nightsString = Transformations.map(nights) { nights ->
+		formatNights(nights, application.resources)
+	}
+	val startButtonVisible = Transformations.map(tonight) {
+		null == it
+	}
+	val stopButtonVisible = Transformations.map(tonight) {
+		null != it
+	}
+	val clearButtonVisible = Transformations.map(nights) {
+		it?.isNotEmpty()
+	}
 
-    private val nights = database.getAllNights()
+	private var _showSnackbarEvent = MutableLiveData<Boolean?>()
 
-    /**
-     * Converted nights to Spanned for displaying.
-     */
-    val nightsString = Transformations.map(nights) { nights ->
-        formatNights(nights, application.resources)
-    }
+	val showSnackBarEvent: LiveData<Boolean?>
+		get() = _showSnackbarEvent
 
-    /**
-     * If tonight has not been set, then the START button should be visible.
-     */
-    val startButtonVisible = Transformations.map(tonight) {
-        null == it
-    }
+	private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
 
-    /**
-     * If tonight has been set, then the STOP button should be visible.
-     */
-    val stopButtonVisible = Transformations.map(tonight) {
-        null != it
-    }
+	val navigateToSleepQuality: LiveData<SleepNight>
+		get() = _navigateToSleepQuality
 
-    /**
-     * If there are any nights in the database, show the CLEAR button.
-     */
-    val clearButtonVisible = Transformations.map(nights) {
-        it?.isNotEmpty()
-    }
+	fun doneShowingSnackbar() {
+		_showSnackbarEvent.value = null
+	}
 
-    /**
-     * Request a toast by setting this value to true.
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
-     */
-    private var _showSnackbarEvent = MutableLiveData<Boolean?>()
+	fun doneNavigating() {
+		_navigateToSleepQuality.value = null
+	}
 
-    /**
-     * If this is true, immediately `show()` a toast and call `doneShowingSnackbar()`.
-     */
-    val showSnackBarEvent: LiveData<Boolean?>
-        get() = _showSnackbarEvent
+	init {
+		initializeTonight()
+	}
 
-    /**
-     * Variable that tells the Fragment to navigate to a specific [SleepQualityFragment]
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
-     */
-    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+	/**
+	 * DB operations
+	 */
+	private fun initializeTonight() {
+		viewModelScope.launch {
+			tonight.value = getTonightFromDatabase()
+		}
+	}
 
-    /**
-     * If this is non-null, immediately navigate to [SleepQualityFragment] and call [doneNavigating]
-     */
-    val navigateToSleepQuality: LiveData<SleepNight>
-        get() = _navigateToSleepQuality
+	private suspend fun getTonightFromDatabase(): SleepNight? {
+		var night = database.getTonight()
+		if (night?.endTimeMilli != night?.startTimeMilli) {
+			night = null
+		}
+		return night
+	}
 
-    /**
-     * Call this immediately after calling `show()` on a toast.
-     *
-     * It will clear the toast request, so if the user rotates their phone it won't show a duplicate
-     * toast.
-     */
-    fun doneShowingSnackbar() {
-        _showSnackbarEvent.value = null
-    }
+	private suspend fun insert(night: SleepNight) {
+		database.insert(night)
+	}
 
-    /**
-     * Call this immediately after navigating to [SleepQualityFragment]
-     *
-     * It will clear the navigation request, so if the user rotates their phone it won't navigate
-     * twice.
-     */
-    fun doneNavigating() {
-        _navigateToSleepQuality.value = null
-    }
+	private suspend fun update(night: SleepNight) {
+		database.update(night)
+	}
 
-    init {
-        initializeTonight()
-    }
+	private suspend fun clear() {
+		database.clear()
+	}
 
-    private fun initializeTonight() {
-        viewModelScope.launch {
-            tonight.value = getTonightFromDatabase()
-        }
-    }
+	fun onStart() {
+		viewModelScope.launch {
+			// Create a new night, which captures the current time,
+			// and insert it into the database.
+			val newNight = SleepNight()
 
-    /**
-     *  Handling the case of the stopped app or forgotten recording,
-     *  the start and end times will be the same.j
-     *
-     *  If the start time and end time are not the same, then we do not have an unfinished
-     *  recording.
-     */
-    private suspend fun getTonightFromDatabase(): SleepNight? {
-            var night = database.getTonight()
-            if (night?.endTimeMilli != night?.startTimeMilli) {
-                night = null
-            }
-            return night
-    }
+			insert(newNight)
 
-    private suspend fun insert(night: SleepNight) {
-            database.insert(night)
-    }
+			tonight.value = getTonightFromDatabase()
+		}
+	}
 
-    private suspend fun update(night: SleepNight) {
-            database.update(night)
-    }
+	fun onStop() {
+		viewModelScope.launch {
+			// In Kotlin, the return@label syntax is used for specifying which function among
+			// several nested ones this statement returns from.
+			// In this case, we are specifying to return from launch().
+			val oldNight = tonight.value ?: return@launch
 
-    private suspend fun clear() {
-            database.clear()
-    }
+			// Update the night in the database to add the end time.
+			oldNight.endTimeMilli = System.currentTimeMillis()
 
-    /**
-     * Executes when the START button is clicked.
-     */
-    fun onStart() {
-        viewModelScope.launch {
-            // Create a new night, which captures the current time,
-            // and insert it into the database.
-            val newNight = SleepNight()
+			update(oldNight)
 
-            insert(newNight)
+			// Set state to navigate to the SleepQualityFragment.
+			_navigateToSleepQuality.value = oldNight
+		}
+	}
 
-            tonight.value = getTonightFromDatabase()
-        }
-    }
+	fun onClear() {
+		viewModelScope.launch {
+			// Clear the database table.
+			clear()
 
-    /**
-     * Executes when the STOP button is clicked.
-     */
-    fun onStop() {
-        viewModelScope.launch {
-            // In Kotlin, the return@label syntax is used for specifying which function among
-            // several nested ones this statement returns from.
-            // In this case, we are specifying to return from launch().
-            val oldNight = tonight.value ?: return@launch
+			// And clear tonight since it's no longer in the database
+			tonight.value = null
 
-            // Update the night in the database to add the end time.
-            oldNight.endTimeMilli = System.currentTimeMillis()
-
-            update(oldNight)
-
-            // Set state to navigate to the SleepQualityFragment.
-            _navigateToSleepQuality.value = oldNight
-        }
-    }
-
-    /**
-     * Executes when the CLEAR button is clicked.
-     */
-    fun onClear() {
-        viewModelScope.launch {
-            // Clear the database table.
-            clear()
-
-            // And clear tonight since it's no longer in the database
-            tonight.value = null
-
-            // Show a snackbar message, because it's friendly.
-            _showSnackbarEvent.value = true
-        }
-    }
+			// Show a snackbar message, because it's friendly.
+			_showSnackbarEvent.value = true
+		}
+	}
 }
