977aaf715004f4799f54ac36f7dd39ab345e6d0e
Hank Chan <hankchan@cubesole.ai>
2021-03-19 23:54:29 +0800

    Networking exercise 3
    - improve UI/UX

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -21,6 +21,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 
 /**
@@ -30,10 +32,16 @@ class DetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        @Suppress("UNUSED_VARIABLE")
-        val application = requireNotNull(activity).application
+		// Binding
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
+
+		// ViewModel
+		val application = requireNotNull(activity).application
+		val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+		val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+		binding.viewModel = ViewModelProvider(this, viewModelFactory).get(DetailViewModel::class.java)
+
         return binding.root
     }
 }
