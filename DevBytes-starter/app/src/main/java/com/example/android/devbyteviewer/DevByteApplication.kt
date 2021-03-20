51c54b2f9523b990ed25045c91a0ddb3261368cd
Hank Chan <hankchan@cubesole.ai>
2021-03-21 06:25:25 +0800

    WorkManager exercise

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,12 +17,21 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
+
+	private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -32,6 +41,43 @@ class DevByteApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+		delayedInit()
     }
+
+	private fun delayedInit() {
+		applicationScope.launch {
+			Timber.plant(Timber.DebugTree())
+			setupRecurringWork()
+		}
+	}
+
+	/**
+	 * Setup WorkManager background job to fetch new network data daily
+	 */
+	private fun setupRecurringWork() {
+
+		// What should be run
+		val constraints = Constraints.Builder()
+				.setRequiredNetworkType(NetworkType.UNMETERED)
+				.setRequiresBatteryNotLow(true)
+				.setRequiresCharging(true)
+				.apply {
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+						setRequiresDeviceIdle(true)
+					}
+				}
+				.build()
+
+		// When should be run
+		val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+				.setConstraints(constraints)
+				.build()
+
+		// Who should run
+		WorkManager.getInstance().enqueueUniquePeriodicWork(
+				RefreshDataWorker.WORK_NAME,
+				ExistingPeriodicWorkPolicy.KEEP,
+				repeatingRequest
+		)
+	}
 }
